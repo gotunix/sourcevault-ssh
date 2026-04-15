ea2cecb7990c59c62d9d69dc8fac086ab9c9d241
Justin Ovens <jovens@gotunix.net>
2026-04-14 22:34:01 -0400

    chore: add license headers to all source files

--- src/version/version.go
+++ src/version/version.go
@@ -1,3 +1,33 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-FileCopyrightText: 2026 GOTUNIX Networks <code@gotunix.net>
+// SPDX-FileCopyrightText: 2026 Justin Ovens <code@gotunix.net>
+// ----------------------------------------------------------------------------------------------- //
+//                 $$$$$$\   $$$$$$\ $$$$$$$$\ $$\\   $$\ $$\\   $$\ $$$$$$\ $$\\   $$\               //
+//                $$  __$$\ $$  __$$\\__$$  __|$$ |  $$ |$$$\\  $$ |\\_$$  _|$$ |  $$ |              //
+//                $$ /  \\__|$$ /  $$ |  $$ |   $$ |  $$ |$$$$\\ $$ |  $$ |  \\$$\\ $$  |              //
+//                $$ |$$$$\\ $$ |  $$ |  $$ |   $$ |  $$ |$$ $$\\$$ |  $$ |   \\$$$$  /               //
+//                $$ |\\_$$ |$$ |  $$ |  $$ |   $$ |  $$ |$$ \\$$$$ |  $$ |   $$  $$<                //
+//                $$ |  $$ |$$ |  $$ |  $$ |   $$ |  $$ |$$ |\\$$$ |  $$ |  $$  /\\$$\\               //
+//                \\$$$$$$  | $$$$$$  |  $$ |   \\$$$$$$  |$$ | \\$$ |$$$$$$\\ $$ /  $$ |              //
+//                 \\______/  \\______/   \\__|    \\______/ \\__|  \\__|\\______|\\__|  \\__|              //
+// ----------------------------------------------------------------------------------------------- //
+// Copyright (C) GOTUNIX Networks                                                                  //
+// Copyright (C) Justin Ovens                                                                      //
+// ----------------------------------------------------------------------------------------------- //
+// This program is free software: you can redistribute it and/or modify                            //
+// it under the terms of the GNU Affero General Public License as                                  //
+// published by the Free Software Foundation, either version 3 of the                              //
+// License, or (at your option) any later version.                                                 //
+//                                                                                                 //
+// This program is distributed in the hope that it will be useful,                                 //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                                 //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                   //
+// GNU Affero General Public License for more details.                                             //
+//                                                                                                 //
+// You should have received a copy of the GNU Affero General Public License                        //
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.                          //
+// ----------------------------------------------------------------------------------------------- //
+
 // Package version provides build and runtime version information for SourceVault SSH.
 //
 // The internal Version constant follows semantic versioning (MAJOR.MINOR.PATCH).
